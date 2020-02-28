dca44dbb86a47e320684ff45da167e32dfa1d581
hutaoer <hutaoer@gmail.com>
2020-02-28 10:47:22 +0800

    feat: init

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
-const chalk = require('chalk');
-
-const ora = require('ora');
-console.log(chalk.blue.bgRed('hello'));
-
-const snipper = ora(chalk.blue('i love you')).start();
-
-snipper.color = 'red';
-setTimeout(() => {
-  snipper.stop();
-}, 1000)
+#!/usr/bin/env node 
+const cm = require('commander');
+const iq = require('inquirer');
+cm.version('1.0.0', '-v --version');
+cm.option('-a -atest', 'this is test');
+cm.command('init <name>').action((name) => {
+  console.log(`you input name is ${name}`);
+  iq.prompt([
+    {
+      type: 'input',
+      name: 'author',
+      message: 'your name?'
+    },
+    {
+      type: 'input',
+      name: 'age',
+      message: 'how old are you?'
+    }
+  ]).then((result) => {
+    console.log(result)
+  })
+})
+cm.parse(process.argv);
